e2f800b54c74f955f4dccb5fa63b0e66c3465ce5
David Catt <xgoodvibesx@gmail.com>
2021-06-04 14:52:05 +0100

    intiial work

---
+++ app/assets/components/header/header.js
@@ -0,0 +1,9 @@
+import AutoComplete from './headerAutoComplete';
+import MenuToggle from './menuToggle';
+import SearchToggle from './searchToggle';
+
+export default () => {
+  AutoComplete();
+  MenuToggle();
+  SearchToggle();
+};
