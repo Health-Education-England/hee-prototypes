dad4d6da6c386b65a18e58bb5cfde3896bd6ac82
Dawn Taroni <dawn.taroni@hee.nhs.uk>
2022-04-12 10:30:38 +0100

    javascript hee-listing file source

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -3,7 +3,7 @@ import AnchorLinks from '../components/anchorlinks/anchor-links';
 import Cookies from '../components/cookies/cookies';
 import Filter from '../components/filter/filter';
 import FilterTag from '../components/filter-tag/filter-tag';
-import Listing from '../components/listing/listing';
+import Listing from '../components/hee-listing/listing';
 import Media from '../components/media/media';
 import NavMap from '../components/nav-map/nav-map';
 import SubMenu from '../components/submenu/submenu';
