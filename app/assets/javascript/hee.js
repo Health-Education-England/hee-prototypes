3dd1a215f4a641943c5c8ca3e2247d9a6f27e14e
Dawn Taroni <dawn.taroni@hee.nhs.uk>
2022-08-02 15:56:22 +0100

    tidy js file and fix submenu bug

--- app/assets/javascript/hee.js
+++ app/assets/javascript/hee.js
@@ -1,5 +1,6 @@
 // NHSUK-HEE Components
 import Cookies from '../components/nhsuk-hee/nhsuk-hee-cookies/cookies';
+import SubMenu from '../components/nhsuk-hee/nhsuk-hee-header/submenu';
 
 // HEE Components
 import AnchorLinks from '../components/hee/hee-anchorlinks/anchorlinks';
@@ -7,15 +8,13 @@ import Listing from '../components/hee/hee-listing/listing';
 import Media from '../components/hee/hee-media/media';
 import NavMap from '../components/hee/hee-navmap/navmap';
 import Newsletter from '../components/hee/hee-newsletter/newsletter';
-import DetailsCard from '../components/hee/hee-detailscard/detailscard';
+//import DetailsCard from '../components/hee/hee-detailscard/detailscard';
 import Tabs from '../components/hee/hee-tabs/tabs';
 //import Timeline from '../components/hee/hee-timeline/timeline';
 
 // Unsorted components
 import Filter from '../components/filter/filter';
 import FilterTag from '../components/filter-tag/filter-tag';
-import SubMenu from '../components/submenu/submenu';
-
 
 //import './polyfills';
 
