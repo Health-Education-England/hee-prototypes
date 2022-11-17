be84e858be63bd013f467ad2d5d22e3b241070aa
Alex Kirsten <alexander.kirsten@gmail.com>
2022-11-17 14:52:22 +0000

    [release/NWPS-Sprint-26] - Moved listings and listings-item components into new dir structure

--- app/assets/hee/hee.js
+++ app/assets/hee/hee.js
@@ -4,7 +4,7 @@ import SubNav from './scaffolding/header/navigation/subnav';
 
 // HEE Components
 import AnchorLinks from '../components/hee/hee-anchorlinks/anchorlinks';
-import Listing from '../components/hee/hee-listing/listing';
+import Listing from './blocks/content/main/hee-listing/listing';
 import Media from '../components/hee/hee-media/media';
 import NavMap from '../components/hee/hee-navmap/navmap';
 import Newsletter from '../components/hee/hee-newsletter/newsletter';
