e82ffe02747ccb10ea1da32859194fe39ab3cbf8
Dawn Taroni <dawn.taroni@hee.nhs.uk>
2022-11-18 13:56:56 +0000

    Move listing blocks to collections furniture folder

--- app/assets/hee/hee.js
+++ app/assets/hee/hee.js
@@ -4,7 +4,7 @@ import SubNav from './scaffolding/header/navigation/subnav';
 
 // HEE Components
 import AnchorLinks from '../components/hee/hee-anchorlinks/anchorlinks';
-import Listing from '../components/hee/hee-listing/listing';
+import Listing from './blocks/furniture/collections/hee-listing/listing';
 import Media from '../components/hee/hee-media/media';
 import NavMap from '../components/hee/hee-navmap/navmap';
 import Newsletter from '../components/hee/hee-newsletter/newsletter';
@@ -13,8 +13,8 @@ import Tabs from '../components/hee/hee-tabs/tabs';
 //import Timeline from '../components/hee/hee-timeline/timeline';
 
 // Unsorted components
-import Filter from '../components/hee/hee-filter/filter';
-import FilterTag from '../components/hee/hee-filtertag/filtertag';
+import Filter from './blocks/furniture/collections/hee-filter/filter';
+import FilterTag from './blocks/furniture/collections/hee-filtertag/filtertag';
 
 //import './polyfills';
 
