32768a9f02dd21370b9576f3ab4617f46276601d
smanivasag <35143542+smanivasag@users.noreply.github.com>
2023-01-19 16:58:13 +0000

    [NWPS-1362] Removal of source-map from hee.min.js (#152)
    * [NWPS-1362] Removal of source-map from hee.min.js
    
    * [Github Actions] - Bundled assets for build number: 223
    
    * [NWPS-1362] Config prototype script generation task to run in 'development' mode (so as to include source-map)
    
    * [Github Actions] - Bundled assets for build number: 224
    
    Co-authored-by: smanivasag <smanivasag@users.noreply.github.com>

--- tasks/assets.js
+++ tasks/assets.js
@@ -65,7 +65,7 @@ function compileHEEScriptsDev() {
 function compilePrototypeScripts() {
   return webpackBuild(
     config.PATHS.prototypeJS,
-    'production',
+    'development',
     'prototype.min.js',
     config.PATHS.public + '/js'
   );
@@ -111,7 +111,7 @@ function webpackBuild(src, mode, filename, dest) {
   return gulp.src(src)
     .pipe(webpack({
       mode: mode,
-      devtool: 'inline-source-map',
+      devtool: mode === 'development' ? 'inline-source-map' : false,
       module: {
         rules: [
           {
