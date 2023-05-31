77450ca6915573ef88067f60bd62d9095c8395bd
Alex Kirsten <alexander.kirsten@gmail.com>
2023-05-31 10:09:36 +0100

    [NWPS-1237] - Amended gitignore and removed engine_scripts dependencies

--- tests/backstop/backstop_data/engine_scripts/playwright/focusState.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (page, scenario) => {
-  const focusSelector = scenario.focusSelector;
-
-  if (focusSelector) {
-    await page.waitForSelector(focusSelector);
-    await page.focus(focusSelector);
-  }
-}
--- tests/backstop/backstop_data/engine_scripts/playwright/onReady.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (page, scenario, viewport, isReference, browserContext) => {
-  console.log('SCENARIO > ' + scenario.label);
-  await require('./clickAndHoverHelper')(page, scenario);
-
-  // Custom helper to set focus on page elements.
-  await require('./focusState')(page, scenario);
-};
