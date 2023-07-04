09f2e624a1af83aacf684f9bd2d43289a2373855
Alex Kirsten <alexander.kirsten@gmail.com>
2023-07-04 15:34:31 +0100

    [NWPS-1566] - Implemented Media component JS test

--- tests/playwright/specs/media.spec.js
+++ tests/playwright/specs/media.spec.js
@@ -1,8 +1,16 @@
 import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
+test('transcript expand and collapse', async ({ page }) => {
   await page.goto('/blocks/content/examples/main-media-embed.html');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Examples/);
+  const transcriptAnchor = page.locator('.nhsuk-media .nhsuk-media__transcript a').first();
+  const transcriptContainer = page.locator('.nhsuk-media .nhsuk-media__transcript').first();
+
+  // Expand media transcript.
+  await transcriptAnchor.click();
+  await expect(transcriptContainer).toHaveClass('nhsuk-media__transcript nhsuk-media__transcript-expanded');
+
+  // Collapse media transcript.
+  await transcriptAnchor.click();
+  await expect(transcriptContainer).toHaveClass('nhsuk-media__transcript');
 });
