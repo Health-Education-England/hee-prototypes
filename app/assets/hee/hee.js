0d9326494e923288a6f51e4281bd64b479c0b77e
Alex Kirsten <alexander.kirsten@gmail.com>
2023-07-13 12:26:35 +0100

    [NWPS-1569] - Moved hee-newsletter to correct location

--- app/assets/hee/hee.js
+++ app/assets/hee/hee.js
@@ -10,7 +10,7 @@ import Listing from './blocks/furniture/collections/hee-listing/listing';
 import Media from './blocks/content/main/hee-media/media';
 import MegaMenu from './blocks/scaffolding/mega-menu/mega-menu';
 import NavMap from './blocks/content/main/hee-navmap/navmap';
-import Newsletter from '../components/hee/hee-newsletter/newsletter';
+import Newsletter from './blocks/content/main/hee-newsletter/newsletter';
 import SummaryCard from "./blocks/content/main/card--summary/summary";
 import Tabs from './blocks/content/main/hee-tabs/tabs';
 import TableCard from './blocks/content/main/table-expander/table-expander';
