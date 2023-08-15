1ba2d09a42b96ef5f460a8cf772e56315137d285
Alex Kirsten <alexander.kirsten@gmail.com>
2023-08-15 13:02:59 +0100

    [NWPS-1568-1569-157] - Merged release/NWPS-1568-1569-157 into develop branch

--- app/assets/hee/hee.js
+++ app/assets/hee/hee.js
@@ -8,9 +8,10 @@ import AnchorLinksSticky from './blocks/content/footer/anchorlinks-sticky/anchor
 import TableContents from './blocks/content/sidebar/anchorlinks/toc';
 import Listing from './blocks/furniture/collections/hee-listing/listing';
 import Media from './blocks/content/main/hee-media/media';
+import MegaMenu from './blocks/scaffolding/mega-menu/mega-menu';
 import NavMap from './blocks/content/main/hee-navmap/navmap';
 import Newsletter from './blocks/content/main/hee-newsletter/newsletter';
-//import SummaryCard from "./blocks/content/main/card--summary/summary";
+import SummaryCard from "./blocks/content/main/card--summary/summary";
 import Tabs from './blocks/content/main/hee-tabs/tabs';
 import TableCard from './blocks/content/main/table-expander/table-expander';
 
@@ -28,9 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
   FilterTag();
   Listing();
   Media();
+  MegaMenu();
   NavMap();
   SubNav();
-  //SummaryCard();
+  SummaryCard();
   Newsletter();
   TableContents();
   TableCard();
