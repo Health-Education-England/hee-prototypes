563a1484d68ada6046339b7d90d97fe0fa94702a
Alex Kirsten <alexander.kirsten@gmail.com>
2023-08-23 15:58:45 +0100

    [NWPS-1672] - Amended playwright test for listings component

--- tests/playwright/specs/listing.spec.js
+++ tests/playwright/specs/listing.spec.js
@@ -4,9 +4,9 @@ test('Listings: sort by selection', async ({ page }) => {
   await page.goto('/blocks/furniture/examples/results-listings.html');
 
   // Check submit button is not visible as per javascript logic.
-  await expect(page.locator('.hee-listing__filter__submit')).not.toBeVisible();
+  await expect(page.locator('.hee-listing__filter__submit').first()).not.toBeVisible();
 
   // Change sort by option and expect page reload.
-  await page.getByLabel('Sort by').selectOption('relevance');
-  await expect(page.locator('.hee-listing__filter__submit')).not.toBeVisible();
+  await page.getByLabel('Sort by').first().selectOption('relevance');
+  await expect(page.locator('.hee-listing__filter__submit').first()).not.toBeVisible();
 });
