d4c20fbcdef49da6b095be145d04aaf2b893349f
Alex Kirsten <alexander.kirsten@gmail.com>
2023-09-11 16:35:43 +0100

    [NWPS-1586] - Adjusted playwright tests

--- tests/playwright/specs/filter.spec.js
+++ tests/playwright/specs/filter.spec.js
@@ -3,21 +3,27 @@ import { test, expect } from "@playwright/test";
 test('Filters: selection and clear', async ({ page }) => {
   await page.goto('/templates/examples/news-collection.html');
 
+  const pageHeader = page.locator('.page__header');
+
   // Ensure submit button is not visible on page load as per javascript logic.
   await expect(page.locator('.hee-listing__filter__submit')).not.toBeVisible();
+
+  await expect(pageHeader).toBeInViewport();
 
   // Uncheck checkbox and expect page reload with results anchor in URL.
-  await page.getByLabel('Graduates').setChecked(false);
+  await page.getByLabel('Careers').click();
 
   // Check URL to ensure parameters and results has are present.
-  await expect(page).toHaveURL(new RegExp('.html\\?filter=learning_dev#results$'));
+  await expect(page).toHaveURL(new RegExp('.html\\?filter=careers&filter=cosmetic-procedures&filter=medicines-optimisation&filter=healthcare-scientists&filter=management-professionals&results_updated=true$'));
 
   // Ensure submit button is not visible on page load as per javascript logic.
   await expect(page.locator('.hee-listing__filter__submit')).not.toBeVisible();
 
+  await expect(pageHeader).not.toBeInViewport();
+
   // Click the clear all link.
-  await page.getByRole('link', {name: 'Clear'}).click();
+  await page.getByRole('link', {name: 'Clear'}).first().click();
 
   // Ensure all parameters have been removed and the results hash is present.
-  await expect(page).toHaveURL(new RegExp('.html\\?#results$'));
+  await expect(page).toHaveURL(new RegExp('.html\\?filter=healthcare-scientists&filter=management-professionals&results_updated=true$'));
 });
--- tests/playwright/specs/listing.spec.js
+++ tests/playwright/specs/listing.spec.js
@@ -3,15 +3,21 @@ import { test, expect } from "@playwright/test";
 test('Listings: sort by selection', async ({ page }) => {
   await page.goto('/templates/examples/news-collection.html');
 
+  const pageHeader = page.locator('.page__header');
+
   // Ensure submit button is not visible on page load as per javascript logic.
   await expect(page.locator('.hee-listing__filter__submit')).not.toBeVisible();
+
+  await expect(pageHeader).toBeInViewport();
 
   // Change sort by option and expect page reload.
   await page.getByLabel('Sort by').selectOption('relevance');
 
   // Check URL to ensure parameters are present.
-  await expect(page).toHaveURL(new RegExp('sort=relevance#results$'));
+  await expect(page).toHaveURL(new RegExp('sort=relevance&results_updated=true$'));
 
   // Ensure submit button is not visible on page load as per javascript logic.
   await expect(page.locator('.hee-listing__filter__submit')).not.toBeVisible();
+
+  await expect(pageHeader).not.toBeInViewport();
 });
