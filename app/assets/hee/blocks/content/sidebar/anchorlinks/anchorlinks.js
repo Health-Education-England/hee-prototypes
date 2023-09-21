c3e78bcbde2a6cb80776c5b5c9fb409743f7d530
Alex Kirsten <alexander.kirsten@gmail.com>
2023-09-21 11:05:10 +0100

    Revert "Revert "[NWPS-1646] - Fixed anchor link generation by restricting to main content area""
    This reverts commit 11a55e1d8de84537efad7767fca849435bef9919.

--- app/assets/hee/blocks/content/sidebar/anchorlinks/anchorlinks.js
+++ app/assets/hee/blocks/content/sidebar/anchorlinks/anchorlinks.js
@@ -24,7 +24,8 @@ export default () => {
     findHeadings(headings) {
       let foundHeadings = []
       if (headings) {
-        document.querySelectorAll(headings).forEach((heading, i) => {
+        const contentContainer = document.querySelector('.page__content');
+        contentContainer.querySelectorAll(headings).forEach((heading, i) => {
           if (!heading.id) {
             heading.id = heading.innerText.replace(/ .*/,'').replace(/[\n\r]/g,'').replace(/\s/g,'').toLowerCase()+i;
           }
