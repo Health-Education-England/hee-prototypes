ceb2ac8a3560aa2b119aeb6bef8c15472a6bdd8f
Selvandhan Manivasagam <selvandhan.manivasagam@tpximpact.com>
2023-10-23 12:29:05 +0100

    [NWPS-1757] [WIP] POC build for (publication) collection page containing taxonomy-based facets

--- app/assets/hee/blocks/furniture/collections/hee-filter/filter.js
+++ app/assets/hee/blocks/furniture/collections/hee-filter/filter.js
@@ -8,9 +8,12 @@ export default () => {
       this.container = container;
 
       this.checkboxes = [...this.container.getElementsByClassName('nhsuk-checkboxes__input')];
+      this.dropdowns = [...this.container.getElementsByClassName('nhsuk-select')];
       this.groups = [...this.container.getElementsByClassName('nhsuk-filter__group')];
+      this.levelTwoGroups = [...this.container.getElementsByClassName('nhsuk-filter__group level-two')];
       this.submit = this.container.querySelector('.nhsuk-filter__submit');
       this.clearToggle = [...this.container.getElementsByClassName('nhsuk-filter__group__clear')];
+      this.clearToggleSelectboxes = [...this.container.getElementsByClassName('nhsuk-filter__group__clear for-select')];
 
       this.setUp();
       this.addEventListeners();
@@ -21,26 +24,42 @@ export default () => {
         checkbox.addEventListener('change', evt => this.updateResults(evt));
       });
 
+      this.dropdowns.forEach(dropdown => {
+        dropdown.addEventListener('change', evt => this.updateResults(evt));
+      });
+
       this.groups.forEach(group => {
         const legend = group.querySelector('.nhsuk-fieldset__legend');
         if (legend) {
-          legend.addEventListener('click', evt => this.toggleGroup(evt));
+          const groupElement = legend.closest('.nhsuk-filter__group');
+          if (!groupElement.classList.contains('level-two')) {
+            legend.addEventListener('click', evt => this.toggleGroup(evt));
+          }
         }
       });
 
       this.clearToggle.forEach(toggle => {
         toggle.addEventListener('click', evt => this.clearCheckboxes(evt));
       });
+
+      this.clearToggleSelectboxes.forEach(toggle => {
+        toggle.addEventListener('click', evt => this.clearSelectboxes(evt));
+      });
     }
 
     setUp() {
       this.container.classList.add('nhsuk-filter--js');
 
-      // Close groups
-      // this.groups.forEach(group => group.classList.add('nhsuk-filter__group--closed'));
+      /* this.collapseAllGroups();
+
+      this.openActiveGroups(); */
 
       this.clearToggle.forEach(toggle => {
         this.toggleClearLinkVisibility(toggle);
+      });
+
+      this.clearToggleSelectboxes.forEach(toggle => {
+        this.toggleClearLinkVisibilityForSelectboxes(toggle);
       });
 
       // Hide submit button
@@ -49,9 +68,29 @@ export default () => {
       }
     }
 
+    /* collapseAllGroups() {
+      ...
+    }
+
+    openActiveGroups() {
+      ...
+    } */
+
     toggleGroup(evt) {
       evt.preventDefault();
-      evt.target.closest('.nhsuk-filter__group').classList.toggle('nhsuk-filter__group--closed');
+      const groupElement = evt.target.closest('.nhsuk-filter__group');
+
+      if (groupElement.getElementsByClassName('nhsuk-select').length > 0) {
+        groupElement.classList.toggle('nhsuk-filter__group--closed');
+
+        const adjacentLevel2Element = groupElement.nextElementSibling;
+        if (adjacentLevel2Element.classList.contains('nhsuk-filter__group')
+          && adjacentLevel2Element.classList.contains('level-two')) {
+          adjacentLevel2Element.classList.toggle('nhsuk-filter__group--level-two-closed');
+        }
+      } else {
+        groupElement.classList.toggle('nhsuk-filter__group--closed');
+      }
     }
 
     setUpdatedFlag(isUpdated) {
@@ -84,12 +123,34 @@ export default () => {
       this.updateResults(evt);
     }
 
+    clearSelectboxes(evt) {
+      evt.preventDefault();
+
+      const toggleLink = evt.target;
+      const selectbox = [...toggleLink.parentElement.querySelectorAll('.nhsuk-select')];
+
+      selectbox.forEach(cb => {
+        cb.selectedIndex = 0;
+      });
+
+      this.updateResults(evt);
+    }
+
     toggleClearLinkVisibility(toggleLink) {
       const checkboxes = [...toggleLink.parentElement.querySelectorAll('.nhsuk-checkboxes__input')];
       for (let i = 0; i < checkboxes.length; i++ ) {
         if (checkboxes[i].hasAttribute('checked')) {
           toggleLink.classList.add('visible');
           break;
+        }
+      }
+    }
+
+    toggleClearLinkVisibilityForSelectboxes(toggleLink) {
+      const selectboxes = [...toggleLink.parentElement.querySelectorAll('.nhsuk-select')];
+      for (let i = 0; i < selectboxes.length; i++ ) {
+        if (selectboxes[i].value !== '') {
+          toggleLink.classList.add('visible');
         }
       }
     }
