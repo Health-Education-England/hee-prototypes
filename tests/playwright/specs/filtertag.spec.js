c909e852878ce0f721bf457f44761d28756bccb8
Alex Kirsten <alexander.kirsten@gmail.com>
2023-10-26 15:22:21 +0100

    [NWPS-1729] - Updated playwright tests for select drop down end to end testing and filter tags

--- tests/playwright/specs/filtertag.spec.js
+++ tests/playwright/specs/filtertag.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test('Filter tags: tag removal', async ({ page }) => {
-  await page.goto('/templates/examples/news-collection.html');
+  await page.goto('/templates/examples/publications-collection.html');
 
   const pageHeader = page.locator('.page__header');
 
@@ -10,12 +10,24 @@ test('Filter tags: tag removal', async ({ page }) => {
 
   await expect(pageHeader).toBeInViewport();
 
-  // Click the Graduates filter tag.
-  const tag = page.locator('div[data-filter="cosmetic-procedures"]');
+  // Click the Dental Professionals filter tag.
+  let tag = page.locator('div[data-filter="dental_professionals"]');
   await tag.click();
 
   // Check resulting URL.
-  await expect(page).toHaveURL(new RegExp('.html\\?filter=medicines-optimisation&filter=healthcare-scientists&filter=management-professionals&results_updated=true$'));
+  await expect(page).toHaveURL(new RegExp('.html\\?profession=&sub_profession=dental_nurse&filter=consultation&filter=guidance&results_updated=true$'));
+
+  // Ensure submit button is not visible on page load as per javascript logic.
+  await expect(page.locator('.hee-listing__filter__submit')).not.toBeVisible();
+
+  await expect(pageHeader).not.toBeInViewport();
+
+  // Click the Graduates filter tag.
+  tag = page.locator('div[data-filter="consultation"]');
+  await tag.click();
+
+  // Check resulting URL.
+  await expect(page).toHaveURL(new RegExp('.html\\?profession=dental_professionals&sub_profession=dental_nurse&filter=guidance&results_updated=true$'));
 
   // Ensure submit button is not visible on page load as per javascript logic.
   await expect(page.locator('.hee-listing__filter__submit')).not.toBeVisible();
