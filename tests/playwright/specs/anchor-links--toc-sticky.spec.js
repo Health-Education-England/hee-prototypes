ec98b41982a52d5882dd14c15efb2abe66b0a394
Alex Kirsten <alexander.kirsten@gmail.com>
2023-11-28 15:38:14 +0000

    [NWPS-1820] - Amended sticky toc playwright test

--- tests/playwright/specs/anchor-links--toc-sticky.spec.js
+++ tests/playwright/specs/anchor-links--toc-sticky.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test('Anchor links: TOC sticky navigation', async ({ page }) => {
-  await page.goto('/blocks/content/examples/footer-sticky-toc.html');
+  await page.goto('/templates/examples/publications-item.html');
 
   const tocSideBarHeading = page.locator('.page__rightbar .hee-anchorlinks h2');
 
@@ -9,8 +9,10 @@ test('Anchor links: TOC sticky navigation', async ({ page }) => {
   const stickyTocBtn = page.getByRole('button', { name: 'Table of Contents' });
   const stickyTocList = page.locator('.hee-anchorlinks__sticky__container .hee-anchorlinks ul');
 
-  // Scroll down to bottom of page.
-  await page.locator('#hee-toc-heading-2-0').click();
+  const footerText = page.locator('.nhsuk-footer__copyright');
+
+  // Scroll down to middle of page.
+  await page.locator('#hee-toc-heading-1-0').click();
 
   // Ensure sidebar links not in viewport and sticky nav is visible.
   await expect(tocSideBarHeading).not.toBeInViewport();
@@ -18,15 +20,30 @@ test('Anchor links: TOC sticky navigation', async ({ page }) => {
 
   await stickyTocBtn.click();
   await expect(stickyTocList).toHaveText([
-    'OverviewPathRun-throughST1-ST7Uncoupled postsTraining routesCore specialty Training (CT1, CT2 (CT3))Higher specialty Training (ST3-ST7 (ST8))',
+    'OverviewPathRun-throughST1-ST7Uncoupled postsTraining routesCore specialty Training (CT1, CT2 (CT3))Higher specialty Training (ST3-ST7 (ST8))SupportIn-training supportProfessional Support and Wellbeing (PSW)Regions',
     'Run-throughST1-ST7Uncoupled posts',
-    'Core specialty Training (CT1, CT2 (CT3))Higher specialty Training (ST3-ST7 (ST8))'
+    'Core specialty Training (CT1, CT2 (CT3))Higher specialty Training (ST3-ST7 (ST8))',
+    'In-training supportProfessional Support and Wellbeing (PSW)'
   ]);
 
   // Scroll back to top of page.
-  await page.getByText('Sticky Table of Contents').click();
+  await page.locator('#publication-title').click();
 
   // Ensure sidebar links in viewport and sticky nav isn't visible.
+  await expect(tocSideBarHeading).toBeInViewport();
+  await expect(stickyToc).not.toBeVisible();
+
+  // Scroll down to footer.
+  await footerText.click();
+
+  // Ensure sticky TOC not visible and footer is visible.
+  await expect(footerText).toBeInViewport();
+  await expect(stickyToc).not.toBeVisible();
+
+  // Scroll to middle of page.
+  await page.locator('#hee-toc-heading-1-0').click();
+
+  // Ensure sidebar links not in viewport and sticky nav is visible.
   await expect(tocSideBarHeading).toBeInViewport();
   await expect(stickyToc).not.toBeVisible();
 });
