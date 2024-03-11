116c200458d767e4b99c31ba941307c191ac0bc0
smanivasag <selvandhan.manivasagam@tpximpact.com>
2024-03-11 19:53:42 +0000

    [NWPS-1820] (1) Removed the duplicate test scenario of scrolling down to the middle of the page and verifying TOC visibility, etc (2) Added a test scenario to close the TOC (by pressing the TOC button) and verify the TOC list visibility.

--- tests/playwright/specs/anchor-links--toc-sticky.spec.js
+++ tests/playwright/specs/anchor-links--toc-sticky.spec.js
@@ -7,7 +7,8 @@ test('Anchor links: TOC sticky navigation', async ({ page }) => {
 
   const stickyToc = page.locator('#hee-anchorlinks-sticky');
   const stickyTocBtn = page.getByRole('button', { name: 'Table of Contents' });
-  const stickyTocList = page.locator('.hee-anchorlinks__sticky__container .hee-anchorlinks ul');
+  const stickyTocWrapper = page.locator('.hee-anchorlinks__sticky__container .hee-anchorlinks');
+  const stickyTocList = stickyTocWrapper.locator('ul');
 
   const footerText = page.locator('.nhsuk-footer__copyright');
 
@@ -18,6 +19,7 @@ test('Anchor links: TOC sticky navigation', async ({ page }) => {
   await expect(tocSideBarHeading).not.toBeInViewport();
   await expect(stickyToc).toBeVisible();
 
+  // Click on sticky TOC button and verify the TOC list
   await stickyTocBtn.click();
   await expect(stickyTocList).toHaveText([
     'OverviewPathRun-throughST1-ST7Uncoupled postsTraining routesCore specialty Training (CT1, CT2 (CT3))Higher specialty Training (ST3-ST7 (ST8))SupportIn-training supportProfessional Support and Wellbeing (PSW)Regions',
@@ -25,6 +27,10 @@ test('Anchor links: TOC sticky navigation', async ({ page }) => {
     'Core specialty Training (CT1, CT2 (CT3))Higher specialty Training (ST3-ST7 (ST8))',
     'In-training supportProfessional Support and Wellbeing (PSW)'
   ]);
+
+  // Click on sticky TOC button again to close the TOC list and verify that it isn't visible
+  await stickyTocBtn.click();
+  await expect(stickyTocWrapper).not.toBeInViewport();
 
   // Scroll back to top of page.
   await page.locator('#publication-title').click();
@@ -38,12 +44,5 @@ test('Anchor links: TOC sticky navigation', async ({ page }) => {
 
   // Ensure sticky TOC not visible and footer is visible.
   await expect(footerText).toBeInViewport();
-  await expect(stickyToc).not.toBeVisible();
-
-  // Scroll to middle of page.
-  await page.locator('#hee-toc-heading-1-0').click();
-
-  // Ensure sidebar links not in viewport and sticky nav is visible.
-  await expect(tocSideBarHeading).toBeInViewport();
   await expect(stickyToc).not.toBeVisible();
 });
